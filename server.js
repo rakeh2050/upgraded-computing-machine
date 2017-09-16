3a771f92c7021920247c6957adbc85e849ab9bed
Jatin Arora <jarora4@myseneca.ca>
2017-09-15 23:37:59 -0400

    3 commit

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ var HTTP_PORT = process.env.PORT || 8080;
 var express = require("express");
 var app = express();
 app.use(express.static('public')); 
-var __dirname="C:/Users/Jatin/Documents/WEB 322/web322-app/views/home.html";
+
 // setup a 'route' to listen on the default url path
 function onHttpStart() {
-    console.log("Express http server listening on: " + HTTP_PORT);
+    console.log("Express http server listening on: " + HTTP_PORT+"  "+__dirname);
     return new Promise (function(res,req){
     data_service.initialize().then(function(data){
       console.log(data)
@@ -16,10 +16,10 @@ function onHttpStart() {
 }
 app.get("/",function(request,respond){
     
-    respond.sendFile(path.join(__dirname));
+    respond.sendFile(path.join(__dirname, '../public', 'home.html'));
 });
-app.get("/about",function(request,respond){
-    respond.sendFile(path.join(__dirname+"/views/home.html"));
-});
+//app.get("/about",function(request,respond){
+   // respond.sendFile(path.join(__dirname));
+//});
 // setup http server to listen on HTTP_PORT
 app.listen(HTTP_PORT);
