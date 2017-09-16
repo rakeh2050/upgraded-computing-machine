8f3509bf2b2811247e0ed3134e38fda1057e2b82
Jatin Arora <jarora4@myseneca.ca>
2017-09-15 22:38:12 -0400

    update 1

---
+++ server.js
@@ -0,0 +1,16 @@
+var HTTP_PORT = process.env.PORT || 8080;
+var express = require("express");
+var app = express();
+app.use(express.static('public')); 
+// setup a 'route' to listen on the default url path
+app.get("/", (req, res) => {
+    res.send("Express http server listening on "+HTTP_PORT);
+});
+app.get("/",function(request,respond){
+    respond.sendFile(path.join(__dirname+"/views/home.html"));
+});
+app.get("/about",function(request,respond){
+    respond.sendFile(path.join(__dirname+"/views/home.html"));
+});
+// setup http server to listen on HTTP_PORT
+app.listen(HTTP_PORT);
