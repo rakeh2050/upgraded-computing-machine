2fffe726afc678dbefe461da7f77b6c4550de4ad
Jatin Arora <jarora4@myseneca.ca>
2017-09-17 12:59:12 -0400

    cdscdscs

--- server.js
+++ server.js
@@ -1,20 +1,35 @@
-var HTTP_PORT = process.env.PORT || 8080;
-var express = require("express");
-var app = express();
-app.use(express.static('public')); 
-__dirname="C:/Users/Jatin/Documents/WEB 322/web322-app"
-// setup a 'route' to listen on the default url path
-//app.get("/", (req, res) => {
-   // res.send("Express http server listening on "+HTTP_PORT+"  "+__dirname);
-//});
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
 
-app.get("/",function(request,respond){
-    
-    respond.sendFile(path.join(__dirname, '/views/', 'home.html'));
-    respond.send("Dir - "+__dirname);
-});
-//app.get("/about",function(request,respond){
-   // respond.sendFile(path.join(__dirname));
-//});
-// setup http server to listen on HTTP_PORT
-app.listen(HTTP_PORT);
+// Create a server
+http.createServer( function (request, response) {  
+   // Parse the request containing file name
+   var pathname = url.parse(request.url).pathname;
+   
+   // Print the name of the file for which request is made.
+   console.log("Request for " + pathname + " received.");
+   
+   // Read the requested file content from file system
+   fs.readFile(pathname.substr(1), function (err, data) {
+      if (err) {
+         console.log(err);
+         // HTTP Status: 404 : NOT FOUND
+         // Content Type: text/plain
+         response.writeHead(404, {'Content-Type': 'text/html'});
+      }else {	
+         //Page found	  
+         // HTTP Status: 200 : OK
+         // Content Type: text/plain
+         response.writeHead(200, {'Content-Type': 'text/html'});	
+         
+         // Write the content of the file to response body
+         response.write(data.toString());		
+      }
+      // Send the response body 
+      response.end();
+   });   
+}).listen(8080);
+
+// Console will print the message
+console.log('Server running at http://127.0.0.1:2020/');
