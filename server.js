83f31f9b92d753803b523e5e4e135600f3ecbd79
Jatin Arora <jarora4@myseneca.ca>
2017-09-17 13:19:35 -0400

    dk;sjkdjk;s

--- server.js
+++ server.js
@@ -1,35 +1,13 @@
-var http = require('http');
-var fs = require('fs');
-var url = require('url');
+var express = require('express');
+var app = express();
+var path = require('path');
+app.use(express.static('public')); 
+// viewed at http://localhost:8080
+app.get("/", (req, res) => {
+    res.send("Express http server listening on "+HTTP_PORT+"  "+__dirname);
+});
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
 
-// Create a server
-http.createServer( function (request, response) {  
-   // Parse the request containing file name
-   var pathname = url.parse(request.url).pathname;
-   
-   // Print the name of the file for which request is made.
-   console.log("Request for " + pathname + " received.");
-   
-   // Read the requested file content from file system
-   fs.readFile(pathname.substr(1), function (err, data) {
-      if (err) {
-         console.log(err);
-         // HTTP Status: 404 : NOT FOUND
-         // Content Type: text/plain
-         response.writeHead(404, {'Content-Type': 'text/html'});
-      }else {	
-         //Page found	  
-         // HTTP Status: 200 : OK
-         // Content Type: text/plain
-         response.writeHead(200, {'Content-Type': 'text/html'});	
-         
-         // Write the content of the file to response body
-         response.write(data.toString());		
-      }
-      // Send the response body 
-      response.end();
-   });   
-}).listen(8080);
-
-// Console will print the message
-console.log('Server running at http://127.0.0.1:2020/');
+app.listen(8080);
