b7dd30652323f71d9a36b49a0c4baa4f2961c8f3
Jatin Arora <jarora4@myseneca.ca>
2017-09-17 14:59:25 -0400

    Final Update

--- server.js
+++ server.js
@@ -1,3 +1,16 @@
+/*********************************************************************************
+*  WEB322 – Assignment 02
+*  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  No part *  of this 
+assignment has been copied manually or electronically from any other source 
+*  (including 3rd party web sites) or distributed to other students.
+* 
+*  Name: JATIN ARORA__  Student ID:136897162_ Date: 17 September , 2017_
+*
+*  Online (Heroku) Link: ________________________________________________________
+*
+********************************************************************************/ 
+
+
 var HTTP_PORT = process.env.PORT || 8080;
 var express = require("express");
 var app = express();
@@ -5,36 +18,18 @@ var app = express();
 app.use(express.static(__dirname + '/public/'));
 
 // setup a 'route' to listen on the default url path
-//app.get("/", (req, res) => {
-  //  res.send(");
-//});
+
 console.log("Express http server listening on "+HTTP_PORT);
 app.get("/",function(request,respond){
     respond.sendFile(__dirname + "/views/home.html");
 
 
 });
-//app.get("/about",function(request,respond){
-   // respond.sendFile(path.join(__dirname));
-//});
-// setup http server to listen on HTTP_PORT
+app.get("/about",function(request,respond){
+    respond.sendFile(__dirname + "/views/about.html");
+
+
+});
+
 app.listen(HTTP_PORT);
-
-
-/*
-
-var express = require('express');
-var app = express();
-
-app.get('/', function (req, res) {
-   res.send('Hello World');
-})
-
-var server = app.listen(8081, function () {
-   var host = server.address().address
-   var port = server.address().port
-   
-   console.log("Example app listening at http://%s:%s", host, port)
-})
-*/
 
